74ab68f03d816a6f1dd4cfdb145de79d2976a749
AvanDriel <avandriel1998@gmail.com>
2018-05-16 23:22:43 +0200

    removed tileUtilities

--- src/game.ts
+++ src/game.ts
@@ -1,12 +1,9 @@
 import * as PIXI from "pixi.js";
-declare var TileUtilities: any;
 
 class Game {
-
-  static instance: Game;
-  public pixi: PIXI.Application;
-
   //singleton function 
+  static instance: Game;
+
   static getInstance(): Game {
     if (!Game.instance) { 
       Game.instance = new Game();
@@ -14,25 +11,9 @@ class Game {
     return Game.instance;
   }
 
+  public pixi: PIXI.Application;
+
   private constructor() {
-
-    PIXI.loader
-      .add("res/terrain.png")
-      .add("res/testmap.json")  
-      .load(setup)
-
-      function setup() {
-        let tu = new TileUtilities(PIXI);
-        
-        let world = tu.makeTiledWorld(
-        "res/testmap.json",
-        "res/terrain.png"
-        );
-        
-        }
-
-    // let tu = new TileUtilities(PIXI);
-    // let world = tu.makeTiledWorld("res/testmap.json", "res/terrain.png");
     
     //making the canvas black and setting the canvas full screen
     this.pixi = new PIXI.Application(innerWidth, innerHeight, {
@@ -44,13 +25,11 @@ class Game {
 
     document.body.appendChild(this.pixi.view);
 
-
-    //setting up gameloop
-    requestAnimationFrame(() => this.gameLoop());
+    this.gameLoop()
   }
   
   private gameLoop():void{
-
+    
     requestAnimationFrame(() => this.gameLoop());
   }
 }
