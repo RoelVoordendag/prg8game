8b12d4fd19def653a157017053b4d15e93fa4614
avandriel <avandriel1998@gmail.com>
2018-05-16 18:49:39 +0200

    why isnt it working?? :(

--- README.md
+++ README.md
@@ -1,23 +1,17 @@
 # PRG08 Game
 
-This is a tiny browser based RPG written in typescript for the course `Programming 08`.
+Small Typescript and Pixi game
 
 ## Installation
-
-One can install the development environment by running `npm install`.
+You can install the development environment by running `npm install`.
 
 ### Build
 In order to build the game, run `npm run build`
 
+### Lib links for reference
+https://github.com/kittykatattack/bump
+https://github.com/kittykatattack/gameUtilities
+https://github.com/kittykatattack/tileUtilities
+
 ### Watcher and Webserver
 To run a watcher and a webserver, please run `npm run start`
-
-
-## Level 2 Requirements
-
-For the course, certain design patterns are required, they are listed below.
-
-## Singleton
-An example of a singleton can be found [here](https://github.com/Nickforall/prg8game/blob/master/src/PixiSingleton.ts).
-
-This singleton is used to get the PIXI Application instance, used for rendering to our canvas.
--- src/game.ts
+++ src/game.ts
@@ -1,14 +1,60 @@
-import PixiSingleton from "./PixiSingleton";
+import * as PIXI from "pixi.js";
+declare var TileUtilities: any;
 
 class Game {
-  constructor() {
-    // initialize canvas
-    PixiSingleton.setInstance(new PIXI.Application(800, 600, { backgroundColor: 0x1099bb }))
-    document.body.appendChild(PixiSingleton.getInstance().view);
+
+  static instance: Game;
+  public pixi: PIXI.Application;
+
+  //singleton function 
+  static getInstance(): Game {
+    if (!Game.instance) { 
+      Game.instance = new Game();
+    }
+    return Game.instance;
+  }
+
+  private constructor() {
+
+    PIXI.loader
+      .add("res/terrain.png")
+      .add("res/testmap.json")  
+      .load(setup)
+
+      function setup() {
+        let tu = new TileUtilities(PIXI);
+        
+        let world = tu.makeTiledWorld(
+        "res/testmap.json",
+        "res/terrain.png"
+        );
+        
+        }
+
+    // let tu = new TileUtilities(PIXI);
+    // let world = tu.makeTiledWorld("res/testmap.json", "res/terrain.png");
+    
+    //making the canvas black and setting the canvas full screen
+    this.pixi = new PIXI.Application(innerWidth, innerHeight, {
+      backgroundColor: 0x000000,
+      resolution: window.devicePixelRatio,
+      autoResize: true
+    });
+    PIXI.settings.SCALE_MODE = PIXI.SCALE_MODES.NEAREST;
+
+    document.body.appendChild(this.pixi.view);
 
 
-    console.log("Loaded Game")
+    //setting up gameloop
+    requestAnimationFrame(() => this.gameLoop());
+  }
+  
+  private gameLoop():void{
+
+    requestAnimationFrame(() => this.gameLoop());
   }
 }
 
-window.addEventListener("load", () => new Game())
+window.addEventListener("load", () => {
+  Game.getInstance();
+})
